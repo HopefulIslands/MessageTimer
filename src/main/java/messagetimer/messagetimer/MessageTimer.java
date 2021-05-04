fa03b3e1de369ec40e26d21b1ba1da00bfe85083
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-05-04 15:47:05 -0400

    Message Timer

---
+++ src/main/java/messagetimer/messagetimer/MessageTimer.java
@@ -0,0 +1,17 @@
+package messagetimer.messagetimer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MessageTimer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
